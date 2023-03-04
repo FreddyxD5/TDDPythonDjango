1b2b1cff388d51af08d230026b0de06cb4d9f306
FreddyxD5 <freddyxd5@gmail.com>
2023-03-04 18:14:21 -0500

    First unit test and url mapping

--- todo/tests.py
+++ todo/tests.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
-
+from django.urls import resolve
+from todo.views import home_page
 # Create your tests here.
 
 class SmokeTest(TestCase):
     def test_bad_maths(self):
         self.assertEqual(1+1,2)
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
